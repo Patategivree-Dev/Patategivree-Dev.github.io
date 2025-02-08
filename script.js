82c6e1359486640c368b88182ba8a22898873dd2
PatateGivree <78503994+Patategivree-Dev@users.noreply.github.com>
2025-02-09 00:31:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 // Fonction pour basculer entre les modes
 const modeToggle = document.getElementById('modeToggle');
 const body = document.body;
+const container = document.querySelector('.container');
 const header = document.querySelector('header');
 const projects = document.querySelectorAll('.project');
 const skills = document.querySelectorAll('.skill');
 const footer = document.querySelector('footer');
 
-// Vérifie le mode de préférence dans le stockage local
-const preferredMode = localStorage.getItem('mode') || 'light';
-if (preferredMode === 'dark') {
-    body.classList.add('dark-mode');
-    header.classList.add('dark-mode');
-    footer.classList.add('dark-mode');
-    projects.forEach(project => project.classList.add('dark-mode'));
-    skills.forEach(skill => skill.classList.add('dark-mode'));
-    modeToggle.textContent = '🌞'; // Affiche le soleil en mode sombre
-} else {
-    modeToggle.textContent = '🌙'; // Affiche la lune en mode clair
-}
-
 // Fonction pour appliquer le mode
 modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
+    container.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
     footer.classList.toggle('dark-mode');
     projects.forEach(project => project.classList.toggle('dark-mode'));
@@ -30,9 +19,7 @@ modeToggle.addEventListener('click', () => {
     // Change l'émoji du bouton en fonction du mode
     if (body.classList.contains('dark-mode')) {
         modeToggle.textContent = '🌞'; // Mode clair
-        localStorage.setItem('mode', 'dark'); // Sauvegarde la préférence dans le stockage local
     } else {
         modeToggle.textContent = '🌙'; // Mode sombre
-        localStorage.setItem('mode', 'light'); // Sauvegarde la préférence dans le stockage local
     }
 });
